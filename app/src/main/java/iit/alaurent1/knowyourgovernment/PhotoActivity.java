42295f9eb262d1c17afd7aa57590590ac9418825
AudeLau <aude_laurent@laposte.net>
2017-06-07 10:45:55 -0500

    Initial commit

---
+++ app/src/main/java/iit/alaurent1/knowyourgovernment/PhotoActivity.java
@@ -0,0 +1,106 @@
+package iit.alaurent1.knowyourgovernment;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import static iit.alaurent1.knowyourgovernment.R.id.googleplus;
+import static iit.alaurent1.knowyourgovernment.R.id.location;
+import static iit.alaurent1.knowyourgovernment.R.id.official_address;
+import static iit.alaurent1.knowyourgovernment.R.id.official_email;
+import static iit.alaurent1.knowyourgovernment.R.id.official_party;
+import static iit.alaurent1.knowyourgovernment.R.id.official_phone;
+import static iit.alaurent1.knowyourgovernment.R.id.official_website;
+
+public class PhotoActivity extends AppCompatActivity {
+
+    private static final String TAG = "OfficialActivity";
+    private Official official;
+    private TextView official_office;
+    private TextView official_name;
+    private ImageView official_photo;
+    private TextView location;
+    private String loc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+
+        official_office = (TextView) findViewById(R.id.offcial_office);
+        official_name = (TextView) findViewById(R.id.official_name);
+        official_photo = (ImageView) findViewById(R.id.official_photo);
+        location = (TextView) findViewById(R.id.location);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra("Official")) {
+            official = (Official) intent.getSerializableExtra("Official");
+        } else {
+            Log.e(TAG, "No official passed");
+            finish();
+        }
+        if(intent.hasExtra("Location")){
+            loc = (String) intent.getSerializableExtra("Location");
+        } else {
+            Log.e(TAG, "No location passed");
+        }
+
+        setBackground();
+        setTexts();
+        loadPhoto();
+    }
+
+    private void setBackground(){
+        if (official.getParty().matches("Democrat.*")){
+            findViewById(R.id.constraintIn).setBackgroundColor(getResources().getColor(R.color.colorDemocratic));
+        } else if (official.getParty().matches("Republic.*")){
+            findViewById(R.id.constraintIn).setBackgroundColor(getResources().getColor(R.color.colorRepublican));
+        } else {
+            findViewById(R.id.constraintIn).setBackgroundColor(getResources().getColor(R.color.colorNoParty));
+        }
+    }
+
+    private void setTexts(){
+        official_office.setText((official.getOffice()!=null ? official.getOffice() : "No Data Provided"));
+        official_name.setText((official.getName()!=null ? official.getName() : "No Data Provided"));
+        location.setText(loc);
+    }
+
+    private void loadPhoto(){
+
+        if (official.getPhotoURL() != null && !official.getPhotoURL().isEmpty()) {
+
+            Picasso picasso = new Picasso.Builder(this)
+                    .listener(new Picasso.Listener() {
+                        @Override
+                        public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
+                            final String changedUrl = official.getPhotoURL().replace("http:", "https:");
+
+                            picasso.load(changedUrl)
+                                    .error(R.drawable.img_brokenimage)
+                                    .placeholder(R.drawable.img_placeholder)
+                                    .into(official_photo);
+                        }
+                    })
+                    .build();
+
+            picasso.load(official.getPhotoURL())
+                    .error(R.drawable.img_brokenimage)
+                    .placeholder(R.drawable.img_placeholder)
+                    .into(official_photo);
+        } else {
+
+            Picasso. with(this).load((String) null)
+                    .error(R.drawable.img_brokenimage)
+                    .placeholder(R.drawable.img_missingimage)
+                    .into(official_photo);
+
+        }
+    }
+}
